b49bedaccd52821cc5674ef24ee65a2630ccc1db
pronapro <nakirandaproscovia@gmail.com>
2018-07-17 01:29:12 -0700

    Update readImage.R
    improved

--- readImage.R
+++ readImage.R
@@ -1,6 +1,6 @@
 library(EBImage)
 library(keras)
-
+setwd('C:/Users/my hp/Downloads/Compressed/GROUP-12-RECESS-2018-Code/GROUP-12-RECESS-2018-Code/MSO_img')
 pic1 <- c('p1.jpg','p2.jpg','p3.jpg','p4.jpg','p5.jpg','p6.jpg','p7.jpg','p8.jpg')
 train <-list()
 for (i in 1:8) {
@@ -36,4 +36,13 @@ for (i in 1:4) {
 par(mfrow= c(2,4))
 for (i in 1:8) {
   plot(train[[i]])
+}
+par(mfrow= c(2,4))
+for (i in 1:8) {
+ x= train[[i]]
+ display(x+0.5)
+ plot(x>0.3)
+ y= bwlabel(x>0.2)
+ max(y)
+ summary(max(y))
 }
